838c20b7ef86606ea342081908ff0e06f1cd5f63
Andjez <92543526+Andjez@users.noreply.github.com>
2023-07-12 23:50:06 +0530

    trail.py

--- trail.py
+++ trail.py
@@ -10,29 +10,33 @@
 #-------------------------------------------------------------------------------
 import streamlit as st
 from langchain.vectorstores import FAISS
-from InstructorEmbedding import INSTRUCTOR
-from langchain.embeddings import HuggingFaceInstructEmbeddings
+#from InstructorEmbedding import INSTRUCTOR
+#from langchain.embeddings import HuggingFaceInstructEmbeddings
 
 
-@st.cache_resource
-def instructor_embeddings():
-    instructor_embed = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-base", model_kwargs={"device": "cpu"})
-    return instructor_embed
+#@st.cache_resource
+#def instructor_embeddings():
+#    instructor_embed = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-base", model_kwargs={"device": "cpu"})
+#    return instructor_embed
 
-def ori_data(_file):
-    db = FAISS.load_local(f"/{file.name}", embed)
-    st.success('Database succussfully created!', icon="✅")
-    return db
+#def ori_data(_file):
+#    db = FAISS.load_local(f"/{file.name}", embed)
+#    st.success('Database succussfully created!', icon="✅")
+#    return db
 
-embed = instructor_embeddings()
+#embed = instructor_embeddings()
 newdb = None
 file = st.file_uploader("Choose a file")
 if file is not None:
     # To read file as bytes:
-    newdb = ori_data(file)
+    #3newdb = ori_data(file)
+    fp = tempfile.TemporaryFile()
+    fp.write(file)
+    fp.read()
+    st.write(fp)
 
 
-if newdb:
-    query = st.text_input("what?")
-    dd = db.similarity_search(query)
-    st.write = (dd)
+#if newdb:
+#    query = st.text_input("what?")
+#    dd = db.similarity_search(query)
+#    st.write = (dd)
